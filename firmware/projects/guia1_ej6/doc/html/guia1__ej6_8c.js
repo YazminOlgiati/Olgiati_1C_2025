e5fe449e34881c9ffd52a518cb6500b9d789de7f
Yazmin Olgiati <yazmin.olgiati@ingenieria.uner.edu.ar>
2025-03-27 20:24:05 -0300

    Doxygen guia1_ej6

---
+++ firmware/projects/guia1_ej6/doc/html/guia1__ej6_8c.js
@@ -0,0 +1,10 @@
+var guia1__ej6_8c =
+[
+    [ "gpioConfig_t", "structgpio_config__t.html", "structgpio_config__t" ],
+    [ "LCD_DIGITS", "guia1__ej6_8c.html#a88b7b97779b05be1dda0fac5fe1e6412", null ],
+    [ "N_BITS", "guia1__ej6_8c.html#a10721343217510b1b324cde94652d8ef", null ],
+    [ "app_main", "guia1__ej6_8c.html#a630544a7f0a2cc40d8a7fefab7e2fe70", null ],
+    [ "BCDtoGPIO", "guia1__ej6_8c.html#a86302003853fbe1b2bedf3ba0d82493f", null ],
+    [ "convertToBcdArray", "guia1__ej6_8c.html#a52ee2e8838ceab35f8bdc0f4e58a0dac", null ],
+    [ "displayNumberOnLCD", "guia1__ej6_8c.html#a3896cf02949de53dfbb63fbe0be00839", null ]
+];
